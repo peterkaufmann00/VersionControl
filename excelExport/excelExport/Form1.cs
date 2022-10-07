3505fa6a5bcab2b0e386de50b9b9ae58b3e7c2ff
peterkaufmann00 <peter.kaufmann@stud.uni-corvinus.hu>
2022-10-07 18:47:36 +0200

    Create Excel commit

--- excelExport/excelExport/Form1.cs
+++ excelExport/excelExport/Form1.cs
@@ -21,11 +21,39 @@ namespace excelExport
         {
             InitializeComponent();
             LoadData();
+            //CreateExcel();
         }
 
         private void LoadData()
         {
             Flats = context.Flats.ToList();
+        }
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
+        void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
     }
 }
