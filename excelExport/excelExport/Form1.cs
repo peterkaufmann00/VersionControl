d6f451eb095a7c72a6cbdd246410e0c8e3311378
peterkaufmann00 <peter.kaufmann@stud.uni-corvinus.hu>
2022-10-07 20:32:00 +0200

    Create Table commit

--- excelExport/excelExport/Form1.cs
+++ excelExport/excelExport/Form1.cs
@@ -21,7 +21,7 @@ namespace excelExport
         {
             InitializeComponent();
             LoadData();
-            //CreateExcel();
+            CreateExcel();
         }
 
         private void LoadData()
@@ -41,6 +41,8 @@ namespace excelExport
                 xlWB = xlApp.Workbooks.Add(Missing.Value);
                 xlSheet = xlWB.ActiveSheet;
 
+                CreateTable();
+
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
             }
@@ -53,6 +55,44 @@ namespace excelExport
                 xlApp.Quit();
                 xlWB = null;
                 xlApp = null;
+            }
+        }
+
+        void CreateTable()
+        {
+            string[] headers = new string[]
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+            };
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+                counter++;
             }
         }
     }
