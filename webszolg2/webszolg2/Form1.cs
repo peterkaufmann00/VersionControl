fefb6e37215766f3636ce27bebada1ef4ef113c6
peterkaufmann00 <peter.kaufmann@stud.uni-corvinus.hu>
2022-10-20 19:03:39 +0200

    Rate list commit

--- webszolg2/webszolg2/Form1.cs
+++ webszolg2/webszolg2/Form1.cs
@@ -7,16 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using webszolg2.Entities;
 using webszolg2.MnbServiceReference;
 
 namespace webszolg2
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
             GetWebservice();
+            dataGridView1.DataSource = Rates.ToList();
         }
 
         private void GetWebservice()
