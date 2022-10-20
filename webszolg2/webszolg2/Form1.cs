a331becb7175b73e7eb153c05a5f67a97ac05658
peterkaufmann00 <peter.kaufmann@stud.uni-corvinus.hu>
2022-10-20 18:46:14 +0200

    Get webservice commit

--- webszolg2/webszolg2/Form1.cs
+++ webszolg2/webszolg2/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using webszolg2.MnbServiceReference;
 
 namespace webszolg2
 {
@@ -15,6 +16,21 @@ namespace webszolg2
         public Form1()
         {
             InitializeComponent();
+            GetWebservice();
+        }
+
+        private void GetWebservice()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
