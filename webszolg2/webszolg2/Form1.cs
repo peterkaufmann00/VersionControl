3af274f34194b72a8758ba31e08edf023826a07f
peterkaufmann00 <peter.kaufmann@stud.uni-corvinus.hu>
2022-10-21 10:34:24 +0200

    Process xml commit

--- webszolg2/webszolg2/Form1.cs
+++ webszolg2/webszolg2/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using webszolg2.Entities;
 using webszolg2.MnbServiceReference;
 
@@ -20,20 +21,47 @@ namespace webszolg2
             InitializeComponent();
             GetWebservice();
             dataGridView1.DataSource = Rates.ToList();
+            ProcessXml(GetWebservice());
         }
 
-        private void GetWebservice()
+        private string GetWebservice()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
                 currencyNames = "EUR",
                 startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                endDate = "2020-06-30",
             };
 
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+
+            return result;
+        }
+        
+        private void ProcessXml(string xmlString)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(xmlString);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                {
+                    rate.Value = value / unit;
+                }
+            }
         }
     }
 }
