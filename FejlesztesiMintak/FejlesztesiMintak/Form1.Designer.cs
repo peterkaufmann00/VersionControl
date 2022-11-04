f3c13d4208a603ee1da2933575ab747c24b77934
peterkaufmann00 <peter.kaufmann@stud.uni-corvinus.hu>
2022-11-04 11:08:26 +0100

    Color choice button commit

--- FejlesztesiMintak/FejlesztesiMintak/Form1.Designer.cs
+++ FejlesztesiMintak/FejlesztesiMintak/Form1.Designer.cs
@@ -36,6 +36,7 @@ namespace FejlesztesiMintak
             this.btnCar = new System.Windows.Forms.Button();
             this.btnBall = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnColor = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -86,11 +87,22 @@ namespace FejlesztesiMintak
             this.label1.TabIndex = 3;
             this.label1.Text = "Coming next:";
             // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.Color.Blue;
+            this.btnColor.Location = new System.Drawing.Point(193, 123);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(119, 26);
+            this.btnColor.TabIndex = 4;
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnColor);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnBall);
             this.Controls.Add(this.btnCar);
@@ -110,6 +122,7 @@ namespace FejlesztesiMintak
         private System.Windows.Forms.Button btnCar;
         private System.Windows.Forms.Button btnBall;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnColor;
     }
 }
 
