13cbea0203254a0275209abfb15fb072753648f8
peterkaufmann00 <peter.kaufmann@stud.uni-corvinus.hu>
2022-11-11 23:14:12 +0100

    Save file commit

--- VaR/VaR/Form1.cs
+++ VaR/VaR/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,14 +17,16 @@ namespace VaR
         List<Tick> Ticks;
         PortfolioEntities context = new PortfolioEntities();
         List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        List<decimal> Nyereségek = new List<decimal>();
+        List<decimal> Nyr;
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
-
-            List<decimal> Nyereségek = new List<decimal>();
+            
+            
             int intervalum = 30;
             DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
             DateTime záróDátum = new DateTime(2016, 12, 30);
@@ -35,11 +38,12 @@ namespace VaR
                 Nyereségek.Add(ny);
                 Console.WriteLine(i + " " + ny);
             }
-
+            
             var nyereségekRendezve = (from x in Nyereségek
                                       orderby x
                                       select x)
                                         .ToList();
+            Nyr = nyereségekRendezve;
             MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
         }
 
@@ -65,6 +69,28 @@ namespace VaR
                 value += (decimal)last.Price * item.Volume;
             }
             return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.DefaultExt = "csv";
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                sw.Write("Időszak");
+                sw.Write(";");
+                sw.Write("Nyereség");
+                sw.WriteLine();
+                foreach (var nyr in Nyr)
+                {
+                    sw.Write(Nyr.Count());
+                    sw.Write(";");
+                    sw.Write(nyr);
+                    sw.WriteLine();
+                }
+            }
+
         }
     }
 }
