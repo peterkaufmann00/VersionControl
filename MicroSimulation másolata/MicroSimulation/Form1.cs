26e6cebc2719386214d2820839bbc863d2fe84dc
peterkaufmann00 <peter.kaufmann@stud.uni-corvinus.hu>
2022-12-02 11:04:14 +0100

    Simulation start commit

--- MicroSimulation másolata/MicroSimulation/Form1.cs
+++ MicroSimulation másolata/MicroSimulation/Form1.cs
@@ -26,6 +26,24 @@ namespace MicroSimulation
             Population = GetPopulation(@"C:\Users\User\AppData\Local\Temp\nép.csv");
             BirthProbabilities = GetBirthProbabilities(@"C:\Users\User\AppData\Local\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Users\User\AppData\Local\Temp\halál.csv");
+
+            for (int year = 2005; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+
+                Console.WriteLine(string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
 
         public List<Person> GetPopulation(string csvpath)
