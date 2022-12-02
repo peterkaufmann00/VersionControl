c61cb82cf92a28279951ef8e3721f3a90ee52635
peterkaufmann00 <peter.kaufmann@stud.uni-corvinus.hu>
2022-12-02 21:06:05 +0100

    Simulation step commit

--- MicroSimulation másolata/MicroSimulation/Form1.cs
+++ MicroSimulation másolata/MicroSimulation/Form1.cs
@@ -31,7 +31,7 @@ namespace MicroSimulation
             {
                 for (int i = 0; i < Population.Count; i++)
                 {
-                    
+                    SimStep(year, Population.FirstOrDefault());
                 }
 
                 int nbrOfMales = (from x in Population
@@ -43,6 +43,34 @@ namespace MicroSimulation
                                     select x).Count();
 
                 Console.WriteLine(string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
+        }
+
+        private void SimStep(int year, Person person)
+        {
+            if (!person.IsAlive) return;
+            byte age = (byte)(year - person.BirthYear);
+
+            var pDeath = (from x in DeathProbabilities
+                          where x.Gender == person.Gender && x.Age == age
+                          select x.Probability).FirstOrDefault();
+
+            if (rng.NextDouble() <= pDeath) person.IsAlive = false;
+
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                var pBirth = (from x in BirthProbabilities
+                              where x.Age == age
+                              select x.Probability).FirstOrDefault();
+
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
             }
         }
 
